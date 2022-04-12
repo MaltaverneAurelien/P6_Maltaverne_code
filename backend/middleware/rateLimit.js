8068a16ee7c28355f9485e6f4e53eff813b3efc6
Aurelien <maltaverneaurelien@gmail.com>
2022-04-12 11:42:45 +0200

    Commentaire mise à jour et point virgule

--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limitez chaque adresse IP à 100 requêtes par "window" (ici, par tranche de 15 minutes)
+  max: 5, // Limitez chaque adresse IP à 5 requêtes par "window" (ici, par tranche de 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const passwordValidator = require('../middleware/passwordValidator');
 const userCtrl = require('../controllers/user');
-const limiter = require('../middleware/rateLimit')
+const limiter = require('../middleware/rateLimit');
 
 router.post('/signup', passwordValidator, userCtrl.signup);
 router.post('/login', limiter.loginLimiter, userCtrl.login);
